11e2b7e1cdc492eb2df408caedce6929558114bd
jesper-laptop <jesperbv4@gmail.com>
2023-02-06 17:06:18 +0100

    måndag igen

---
+++ week10/homework10.test.ts
@@ -0,0 +1,45 @@
+import { lg_permute_list, lg_permute_nodeLists, random_permutation, lg_kosaraju, random_kosaraju } from './homework10';
+import { is_null, list, pair , head, tail, List} from '../lib/list';
+import { member, length, for_each, map} from '../lib/list_prelude';
+import { lg_from_edges, EdgeList } from '../lib/graphs';
+import { type Queue, enqueue, empty, is_empty, head as q_head, dequeue} from '../lib/queue_array';
+import exp from 'constants';
+
+function list_to_array<T>(xs: List<T>):Array<T> {
+    const xs_array: Array<T> = [];
+    while(!is_null(xs)) {
+        xs_array.push(head(xs))
+        xs = tail(xs)
+    }
+    return xs_array;
+}
+
+test('Permutation is returning the correct elements in a new order', () => {
+    const A = [0, 1, 2, 3];
+    const perm1 = random_permutation(A.length)
+    const perm2 = random_permutation(A.length)
+    for(let i = 0; i < A.length; i++) {
+        expect(perm1).toContain(i)
+    }
+    expect(perm1.length).toBe(A.length);
+    expect(perm1).not.toStrictEqual(A);
+    expect(perm1).not.toStrictEqual(perm2);
+})
+
+const el:EdgeList = list(pair(0,1), pair(1,2), pair(2,3), pair(3,0),
+                pair(3,4), pair(4,6), 
+                pair(5,6), pair(6,7), pair(7,5));
+
+const lg = lg_from_edges(8, el);
+
+
+test('Has the same SCCs', () => {
+    const scc_list1 = list_to_array(lg_kosaraju(lg)).sort();
+    const scc_list2 = list_to_array(random_kosaraju(lg)).sort();
+    for (let i = 0; i < scc_list1.length; i ++) {
+        const scc1 = list_to_array(scc_list1[i]).sort();
+        const scc2 = list_to_array(scc_list2[i]).sort();
+        expect(scc1).toStrictEqual(scc2)
+    }
+    
+})
--- week10/homework10.ts
+++ week10/homework10.ts
@@ -1,10 +1,72 @@
-import {List, list, pair, is_null, head, tail} from '../lib/list'
-import { length} from '../lib/list_prelude'
-import { enqueue, empty, is_empty, type Prio_Queue, head as qhead, dequeue} from '../lib/prio_queue'
+import { enqueue, empty, is_empty, type Prio_Queue, head as qhead, dequeue } from '../lib/prio_queue'
+import { lg_new, type ListGraph, lg_transpose, lg_from_edges } from '../lib/graphs'
+import { type List, type Pair, pair, tail, head, is_null, set_head, list } from "../lib/list";
+import { build_array, enum_list, for_each, map, length } from "../lib/list_prelude";
 
+
+const white = 1;
+const black = 2;
+const grey  = 3;
+
+function lg_dfs_reverse_finish_order({adj, size}: ListGraph, 
+                                     restart_order: List<number> = null): List<number> {        
+    var result: List<number> = null;
+    const colour = build_array(size, _ => white);
+    if (restart_order === null) {
+        restart_order = enum_list(0, size - 1);
+    } else {}
+
+    function dfs_visit(current: number): void {
+        if (colour[current] === white) {
+            colour[current] = grey;
+            map(dfs_visit, adj[current]);
+            colour[current] = black;
+            result = pair(current, result);
+        } else {}
+    }
+    for_each(dfs_visit, restart_order);
+    return result;
+}
+
+
+function lg_dfs_reachables({adj, size}: ListGraph, 
+                           restart_order: List<number> = null): List<List<number>> {        
+    var result: List<List<number>> = null;
+
+    const colour = build_array(size, _ => white);
+    if (restart_order === null) {
+        restart_order = enum_list(0, size - 1);
+    } else {}
+
+    function dfs_visit(current: number): void {
+    if (colour[current] === white && !is_null(result)) {
+        set_head(result, pair(current, head(result)));
+        colour[current] = grey;
+        map(dfs_visit, adj[current]);
+        colour[current] = black;
+        } else {}
+    }
+
+    function dfs_restart(initial: number): void {
+        if (colour[initial] === white) {
+            result = pair(null, result);
+            dfs_visit(initial);
+        } else {}
+    }
+    for_each(dfs_restart, restart_order);
+    return result;
+}
+
+export function lg_kosaraju(lg: ListGraph, 
+                            restart_order: List<number> = null): List<List<number>> {
+    return lg_dfs_reachables(lg_transpose(lg), 
+                             lg_dfs_reverse_finish_order(lg, restart_order));
+}
+
+//Start of my code
 type Permutation = Array<number>;
 
-function random_permutation(length: number): Permutation {
+export function random_permutation(length: number): Permutation {
 
     function getRandomInt(min: number, max: number): number {
         min = Math.ceil(min);
@@ -29,7 +91,7 @@ function random_permutation(length: number): Permutation {
     return result;
 }
 
-function lg_permute_list<T>(xs: List<T>): List<T> {
+export function lg_permute_list<T>(xs: List<T>): List<T> {
     
     function reorder<T>(xs: List<T>): Prio_Queue<T> {
         const len = length(xs);
@@ -54,6 +116,19 @@ function lg_permute_list<T>(xs: List<T>): List<T> {
     }
     return p_list(reorder(xs));
 }
-//function lg_permute_nodeLists(lg: ListGraph): ListGraph
 
-console.log(lg_permute_list(list(1,2,3,4)));
+export function lg_permute_nodeLists(lg: ListGraph): ListGraph {
+    const res = lg_new(lg.size);
+    for (let i = 0; i < lg.size; i++) {
+        res.adj[i] = lg_permute_list(lg.adj[i]);
+    }
+    return res
+}
+
+export function random_kosaraju(lg: ListGraph): List<List<number>> {
+    const random_nodeLists = lg_permute_nodeLists(lg)
+    const random_restartOrder = lg_permute_list(enum_list(0, lg.size - 1))
+//    console.log(random_nodeLists);      //TEST
+//    console.log(random_restartOrder);   //TEST
+    return  lg_kosaraju(random_nodeLists, random_restartOrder);
+}
